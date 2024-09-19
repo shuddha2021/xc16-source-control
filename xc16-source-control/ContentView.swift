f1012bad9d0e27617f4b62e74a432f31ad704448
Shuddha Chowdhury <shuddha7435@gmail.com>
2024-09-19 13:06:16 -0500

    Initial Commit

---
+++ xc16-source-control/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  xc16-source-control
+//
+//  Created by Shuddha Chowdhury on 9/19/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ xc16-source-control/xc16_source_controlApp.swift
@@ -0,0 +1,17 @@
+//
+//  xc16_source_controlApp.swift
+//  xc16-source-control
+//
+//  Created by Shuddha Chowdhury on 9/19/24.
+//
+
+import SwiftUI
+
+@main
+struct xc16_source_controlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
